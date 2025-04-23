5d04def4079dbd5e62e7a30c8d4bfb668ef90a4b
Barry Dam <info@barrydam.nl>
2025-04-23 07:41:29 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
+import BdOrmDbConnectionCapSqlite from './dbConnections/BdOrmDbConnectionCapSqlite';
 import BaseORMCrud from './_/BdORMCrud';
 import BdOrmDbConnection from './_/BdOrmConnection';
+import BdOrmDbConnectionCapHana from './dbConnections/BdOrmDbConnectionCapHana';
+import BdOrmDbConnectionSQLBase from './dbConnections/BdOrmDbConnectionSQLBase';
 
 export default class BdOrm extends BaseORMCrud {}
-export { BdOrmDbConnection };
+export { BdOrmDbConnection, BdOrmDbConnectionCapSqlite, BdOrmDbConnectionCapHana, BdOrmDbConnectionSQLBase };
